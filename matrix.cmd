9a8f502a5d7f9c1f3c7221c8274583bb6bed2d23
lx78WyY0J5 <9j2k37st@gmail.com>
2022-08-30 11:22:58 +0200

    Add files via upload

---
+++ matrix.cmd
@@ -0,0 +1,9 @@
+@echo off
+:a
+color a
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+goto :a
